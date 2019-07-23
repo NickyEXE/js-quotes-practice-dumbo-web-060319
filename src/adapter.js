17484127fea8c38fb39c73fd8aab54c9c8cf522d
NickyEXE <dovern42@gmail.com>
2019-07-23 12:24:42 -0400

    making comments more clear

--- src/adapter.js
+++ src/adapter.js
@@ -2,8 +2,10 @@ const API = "http://localhost:3000/"
 const API_QUOTES = API + `quotes`
 const API_LIKES =  API + `likes`
 
-adapter = {
+// Since none of these have access to my callback functions (the fetches aren't in the closure where they were defined, we pass through the callback function to be done at the end of a fetch as an argument.)
+// each key value pair here is a name of a function, and then a function.
 
+adapter = {
     getQuotes: (callback) => {
         fetch(`${API_QUOTES}?_embed=likes`)
         .then(res => res.json())
