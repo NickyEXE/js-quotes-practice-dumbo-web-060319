de7a40dbb8ea771c6f719e7886021a9112eb2469
NickyEXE <dovern42@gmail.com>
2019-07-23 12:21:59 -0400

    Built this to showcase the fetch adapter pattern and demonstrate how callbacks can be passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=> {
 
-    ////// CALLBACK FUNCTIONS -- All of these are called after a fetch made by functions defined in the Adapter.js file.
+    ////////////////////////////// CALLBACK FUNCTIONS -- These are called after a fetch made by functions defined in the Adapter.js file. //////////////////////////////
 
     // This callback function takes the list of quotes returned by our API and renders it to the DOM and is called on the getQuotes function in our adapter. 
     // (Only called on initial render. If you feel like reloading all the data after everything the user does, this can also be passed as the callback for every fetch request used in this lab.)
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
     
     // This helper function takes an individual quote object and turns it into an li. Called on the initial render and whenever we post a quote.
     function renderQuote(quote){
-        return ` <li class='quote-card'>
+        return `<li class='quote-card'>
         <blockquote class="blockquote">
           <p class="mb-0">${quote.quote}</p>
           <footer class="blockquote-footer">${quote.author}</footer>
@@ -27,24 +27,18 @@ document.addEventListener('DOMContentLoaded', ()=> {
       </li>`
     }
 
-    // This callback function renders likes given a quote's ID (It is called after submitting the fetch)
+    // This callback function finds a quote on the DOM given its ID, and adds one to its number of likes on the DOM (It is called after submitting the like fetch from the adapter)
     function addLikeById(quoteId){
         button = document.querySelector(`[data-like-id="${quoteId}"]`)
-        // The parseInt portion simply steals the number of likes from the string "Likes: 15" or whatever. 
-        // It would probably be easier to simply make a data-likes=quotes.likes.length in the renderQuote method above and then pull from the dataset here. 
-        // But this is fun too.
         button.innerText = `Likes: ${parseInt(button.innerText.split(": ")[1])+1}`
     }
-    
-    // This callback function optimistically removes a quote (It is called after submitting the fetch)
+
+    // This callback function removes a quote from the DOM given its id (It is called after submitting the delete fetch from the adapter)
     function removeQuoteById(id){
-        // removes the parent element (the li) of the parent element (the quoteblock) of the delete button
         document.querySelector(`[data-delete-id='${id}']`).parentElement.parentElement.remove()
     }
     
-
-    // On submit button click, submits with the relevant data to my adapter's postQuote function, and includes my callback function to add a quote to the DOM. 
-    // Then it clears the form.
+    // On submit button click, submits with the relevant data to my adapter's postQuote function, and includes my callback function to add a quote to the DOM. Then it clears the form.
     function postQuote(e){
         e.preventDefault()
         quote = document.getElementById("new-quote").value
@@ -54,18 +48,19 @@ document.addEventListener('DOMContentLoaded', ()=> {
         document.getElementById("author").value = ""
     }
 
-    ////// DOM functions - Click handler, event listener, and initial render calls.
+    ////////////////////////////// DOM functions - Click handler, event listener, and initial render calls. //////////////////////////////
 
     // Handles clicks and, in the case of a click of the like button or the delete button, calls their relevant adapter function, passing through a callback with the id preloaded.
     function handleClick(e){
         switch (e.target.classList[0]){
-            case "btn-success":
-                // For like and delete, I've loaded up the id in two different ways. In this case (like), I know the API is going to return the relevant quote object on a success, which has a quoteId.
+            // For like and delete, I've loaded up the id in two different ways for funsies.
+            case "btn-success":                
+                // In this case (like), I know the API is going to return the relevant quote object on a success, which has a quoteId.    
                 // When I pass through the callback function (quoteObject) => addLikeById(quote.quoteId)) and call it in the second .then, the response from the fetch is passed to it as an argument.
                 adapter.likeQuote(parseInt(e.target.dataset.likeId), (quoteObject) => addLikeById(quoteObject.quoteId));
                 break;
             case "btn-danger":
-                // These two fetches show two ways to get the ID -- In this case (delete), the API doesn't send back anything, so I pulled the id from the dataset from the target of the event.
+                // In this case (delete), the API doesn't send back anything, so I pulled the id from the dataset from the target of the event.
                 adapter.deleteQuote(e.target.dataset.deleteId, () => removeQuoteById(e.target.dataset.deleteId))
                 break;
             default:
@@ -75,33 +70,6 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     // Initial render and set up event listeners
     adapter.getQuotes(renderQuotes)
-    document.addEventListener("submit", postQuote)
-    document.addEventListener("click", handleClick)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    document.getElementById("new-quote-form").addEventListener("submit", postQuote)
+    document.getElementById("quote-list").addEventListener("click", handleClick)
 })
