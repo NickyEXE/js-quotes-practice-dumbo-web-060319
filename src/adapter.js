d4b7e2d79f34edf18c8f60c0c6ec300212c8a10b
NickyEXE <dovern42@gmail.com>
2019-07-23 12:28:16 -0400

    Cleaning up a couple more comments.

--- src/adapter.js
+++ src/adapter.js
@@ -2,10 +2,10 @@ const API = "http://localhost:3000/"
 const API_QUOTES = API + `quotes`
 const API_LIKES =  API + `likes`
 
-// Since none of these have access to my callback functions (the fetches aren't in the closure where they were defined, we pass through the callback function to be done at the end of a fetch as an argument.)
-// each key value pair here is a name of a function, and then a function.
+// Since none of these have access to my callback functions (the fetches aren't in the closure where they were defined), we pass through as an argument the callback function to be done at the end of a fetch.)
+// Each key value pair here is a name of a function, and then a function. By building it this way in this constant, I can call them anywhere in my program by writing adapter.functionName(args).
 
-adapter = {
+const adapter = {
     getQuotes: (callback) => {
         fetch(`${API_QUOTES}?_embed=likes`)
         .then(res => res.json())
